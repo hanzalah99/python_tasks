6ceea1ebcaaaf59e108f47f57b897de62e4b2f60
hanzalah99 <hanzalahqayyum.ee@gmail.com>
2023-05-17 09:56:09 +0500

    task 1

---
+++ task1.py
@@ -0,0 +1,4 @@
+for x in range(2000,3200):       #for loop is used for the range 2000-3200    
+    if (x%7==0 and x%5!=0):      #if the number is divisble by and not divisible by 5
+         print(x,end=",")        #then print the number with a comma at the end
+        
